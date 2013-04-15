d7aff9738e64811cf3bef14baa3a002619ad8092
Tobias Schlager <tobias.schlager@lindenbaum.eu>
2013-04-15 14:56:11 +0200

    Embed native MD2 implementation provided by RFC 1319.
    This also increases the version number to 1.1.0. Native code was
    embedded since OpenSSL removed MD2 support.

--- README.md
+++ README.md
@@ -8,22 +8,18 @@ Summary
 
 This OTP library application contains an erlang-only implementation as
 well as a NIF-based implementation of MD2. The NIF based implementation
-needs libcryto (from [OpenSSL](http://www.openssl.org/)) to work. If
-the NIF can't be loaded the erlang implementation (slower) will be used.
+is taken from the code provided directly in RFC 1319. If the NIF can't be
+loaded the erlang implementation (slower) will be used.
 
 Building
 --------
 
 The project is built using [rebar](https://github.com/basho/rebar).
 
-To disable compilation and usage of the NIF based implementation (e.g.
-for systems without OpenSSL) just remove the `port_specs` and `port_env`
-configuration in the `rebar.config` file.
-
 To use this project as a dependency, just add the following to the `deps`
 section of your `rebar.config`:
 
-`{md2, "1.0.1", {git, "https://github.com/lindenbaum/md2.git", {tag, "1.0.1"}}}`
+`{md2, "1.1.0", {git, "https://github.com/lindenbaum/md2.git", {tag, "1.1.0"}}}`
 
 Usage
 -----
--- c_src/md2.c
+++ c_src/md2.c
@@ -1,10 +1,10 @@
 /**
  * Copyright (c) 2012 Lindenbaum GmbH
- * 
+ *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
@@ -12,12 +12,13 @@
  * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- * 
- * A small erlang NIF using OpenSSL's MD2 implementation.
+ *
+ * A small erlang NIF using the MD2 implementation as provided by RFC 1319.
  */
 
 #include <string.h>
-#include <openssl/md2.h>
+
+#include "md2.h"
 
 #include "erl_nif.h"
 
@@ -30,9 +31,9 @@ static ERL_NIF_TERM hash(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
   MD2_CTX context;
   unsigned char digest[16];
 
-  MD2_Init(&context);
-  MD2_Update(&context, binary.data, binary.size);
-  MD2_Final(digest, &context);
+  MD2Init(&context);
+  MD2Update(&context, binary.data, binary.size);
+  MD2Final(digest, &context);
 
   ErlNifBinary result;
   if (!enif_alloc_binary(16, &result)) {
@@ -46,4 +47,3 @@ static ERL_NIF_TERM hash(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
 static ErlNifFunc nif_funcs[] = { {"hash", 1, hash} };
 
 ERL_NIF_INIT(md2, nif_funcs, NULL, NULL, NULL, NULL)
-
